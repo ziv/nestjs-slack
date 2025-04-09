46a520acb31851fc69e8a744d13852a33873e326
ziv-perry <ziv.perry@zoominfo.com>
2025-04-09 12:59:50 +0300

    fix: patch for nestjs logger methods

--- packages/nestjs-slack/utils.ts
+++ packages/nestjs-slack/utils.ts
@@ -1,10 +1,10 @@
-import { EventPattern, MessagePattern } from "@nestjs/microservices";
-import { type Logger, LogLevel } from "@slack/bolt";
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
+import { type Logger, LogLevel } from '@slack/bolt';
 
 export type Pattern = string | RegExp;
 
 const normalizePattern = (pattern: unknown): string => {
-  if (typeof pattern === "string") {
+  if (typeof pattern === 'string') {
     return pattern;
   }
   if (pattern instanceof RegExp) {
@@ -51,23 +51,26 @@ export function messageDecorator(
 }
 
 export function adjustLogger(logger: any): Logger {
-  // minimal logger interface required by the library
-  for (const method of ["debug", "info", "warn", "error"]) {
-    if (typeof logger[method] !== "function") {
+  // this patch required to support NestJS logger that not use the standard "info"
+  // and use "log" instead
+  if (logger['log'] && !logger['info']) {
+    logger['info'] = logger['log'].bind(logger);
+  }
+
+  // minimal logger interface required by @slack/bolt library
+  for (const method of ['debug', 'info', 'warn', 'error']) {
+    if (typeof logger[method] !== 'function') {
       throw new Error(`Logger must implement "${method}" method`);
     }
   }
-
-  const noop = () => {
-  };
 
   return Object.assign(logger, {
     setLevel(_: LogLevel) {
       // don't allow library to set the level
     },
     setName(name: string) {
-      // setContext is alias for setName
-      (logger.setName ?? logger.setContext ?? noop)(name);
+      // search for all possible setName/setContext methods, use "noop" if none
+      (logger.setName ?? logger.setContext ?? ((i: string) => i))(name);
     },
     getLevel(): LogLevel {
       // always return the lowest supported level (by library, not real logger)
