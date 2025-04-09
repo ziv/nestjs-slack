9209f69cafa4ed20193eb38f67647c5ba2414bc3
ziv-perry <ziv.perry@zoominfo.com>
2025-04-09 11:57:00 +0300

    done

--- packages/nestjs-slack-assistant/slack-assistant.spec.ts
+++
@@ -1,9 +0,0 @@
-/* sanity */
-import SlackAssistant from "./slack-assistant";
-import { Server } from "@nestjs/microservices";
-
-const t = new SlackAssistant({ slack: { token: "", appToken: "" } });
-
-test("SlackAssistantTransport", () => {
-  expect(t).toBeInstanceOf(Server);
-});
