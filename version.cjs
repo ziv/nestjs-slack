1c20edc9322c4680eaf064cbdb1a51c4029bdc5a
ziv-perry <ziv.perry@zoominfo.com>
2025-04-14 12:56:16 +0300

    release

--- version.cjs
+++ version.cjs
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const argv = require('node:process').argv;
-const inspect = require('node:util').inspect;
 const write = require('node:fs').writeFileSync;
 const nestjsSlack = require('./packages/nestjs-slack/package.json');
 const nestjsSlackAssistant = require('./packages/nestjs-slack-assistant/package.json');
@@ -16,5 +15,5 @@ nestjsSlackAssistant.version = version;
 nestjsSlackAssistant.dependencies['@xpr/nestjs-slack'] = version;
 
 
-write('./packages/nestjs-slack/package.json', inspect(nestjsSlack), 'utf8');
-write('./packages/nestjs-slack-assistant/package.json', inspect(nestjsSlackAssistant), 'utf8');
+write('./packages/nestjs-slack/package.json', JSON.stringify(nestjsSlack, 2), 'utf8');
+write('./packages/nestjs-slack-assistant/package.json', JSON.stringify(nestjsSlackAssistant), 'utf8');
